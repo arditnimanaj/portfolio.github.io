7efd0e489b9c945db20458c8c3a9100ab7970bdf
arditnimanaj1999 <arditnimanaj@gmail.com>
2024-01-14 20:07:38 +0100

    logo and title fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,15 @@ import "./App.css";
 import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className=" bg-[#1F2937] flex">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>ardit.dev</title>
+      </Helmet>
       <SideBar />
       <div className="flex flex-col w-full">
         <Hero />
