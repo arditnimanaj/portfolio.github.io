a70daccbf3529766feb7b6128e275c7c2653d1e7
Ardit Nimanaj <ardit.nimanaj@pabau.com>
2026-03-06 15:31:38 +0100

    fixed dark mode icon

--- src/components/ui/darkModeToggle.tsx
+++ src/components/ui/darkModeToggle.tsx
@@ -8,7 +8,7 @@ interface DarkModeToggleProps {
 export default function DarkModeToggle({ setDarkMode }: DarkModeToggleProps) {
   return (
     <div className="flex items-center">
-      <FaMoon className="text-gray-800 dark:text-gray-200 text-2xl mr-2" />
+      <FaMoon className="text-gray-800 dark:text-gray-200 text-xl mr-2" />
       <Switch onClick={setDarkMode} />
     </div>
   );
